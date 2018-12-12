541c8ae84b82027e645eaea5a726d2c48dd0fd49
AnriNasH <Nashbook@mail.ru>
2018-12-13 02:00:49 +0300

    Домашка 05, easy 02

---
+++ lesson05/lesson05_home-work_easy-task02.py
@@ -0,0 +1,13 @@
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+
+import os
+import shutil
+import sys
+
+def list_dir(main_path):
+    for _ in os.listdir(main_path):
+        print(_)
+main_path = os.getcwd()
+list_dir(main_path)
